3561577bbbeb5b937a9731f65fc39ee6664528c5
Maia <raafamaia@gmail.com>
2017-09-24 23:15:11 -0300

    scaffold done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'my-app',
-    template: '<h1> LEEROY JENKINS </h1>'
+  selector: "my-app",
+  template: "<h1> LEEROY JENKINS </h1>"
 })
-
-export class AppComponent { }
+export class AppComponent {}
